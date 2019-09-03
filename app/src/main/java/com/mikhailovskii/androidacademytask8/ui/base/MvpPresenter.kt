93b4f47dbcdb081bdc2e46ff3c9b21bbdf408dd3
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-09-03 22:57:39 +0300

    Removed MVP structure

--- app/src/main/java/com/mikhailovskii/androidacademytask8/ui/base/MvpPresenter.kt
+++
@@ -1,9 +0,0 @@
-package com.mikhailovskii.androidacademytask8.ui.base
-
-interface MvpPresenter<View> {
-
-    fun attachView(view: View)
-
-    fun detachView()
-
-}
--- app/src/main/java/com/mikhailovskii/androidacademytask8/ui/base/MvpView.kt
+++
@@ -1,7 +0,0 @@
-package com.mikhailovskii.androidacademytask8.ui.base
-
-interface MvpView {
-
-    //Not implemented
-
-}
